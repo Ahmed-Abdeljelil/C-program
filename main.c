3af83d6111b350eacafc49ae3d8d0a6543bf4618
Ahmed-Abdeljelil <abdeljelilahmed501@gmail.com>
2026-09-22 10:58:27 +0900

    Add files via upload

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
-
-int main(){
-    printf("Hello, World!\n");
+int main() {
+    int x;
+    int y;
+    scanf("%d %d",&x , &y);
+    printf("sum is : %d\n", x+y);
+    printf("the difference is: %d\n",x-y);
+    printf("the product is %d\n",x*y);
+    printf("the quotient is: %d\n",x/y);
     return 0;
+    
 }
+
+
